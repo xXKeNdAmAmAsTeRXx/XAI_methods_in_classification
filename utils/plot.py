0836c664526b0bdac72cb96acc97abd3c76c2a85
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2025-11-19 04:33:00 +0100

    vizualzation of Gradients map functions, function plot_with_function, gradCAM and IC vizualziation

--- utils/plot.py
+++ utils/plot.py
@@ -1,5 +1,8 @@
+import warnings
+
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 from torch.utils.data import Subset
 import  PIL.Image as Image
 import torchvision
@@ -67,3 +70,43 @@ def plot_time(epoch_time:list[float])->None:
     plt.xlabel('Epoch')
     plt.ylabel('Time')
     plt.show()
+
+def plot_with_function(idx, preds, dataset, func, transform):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    fig, axes = plt.subplots(2,3,figsize=(20, 8))
+    fig.subplots_adjust(top=1)
+
+    for i, k in enumerate(idx):
+        img, class_num = dataset[k]
+        img_tensor = (transform(img).unsqueeze(0)).cpu()
+        img_tensor.requires_grad_(True)
+
+        # Calculating Grad Cam
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=UserWarning)
+            attribution = func(img_tensor, target=int(preds[i][0]))
+
+        attribution_np = attribution.detach().cpu().numpy()
+        attribution_np = np.max(np.abs(attribution_np), axis=0)
+
+
+        min_val = np.min(attribution_np)
+        max_val = np.max(attribution_np)
+
+        if max_val > min_val:
+            attribution_np = (attribution_np - min_val) / (max_val - min_val)
+
+        attribution_show = attribution_np[0]
+
+        # Getting and resizng orginal image
+        original_img, _ = dataset[k]
+        img_show = Image.fromarray(np.array(original_img))
+        img_show = img_show.convert('RGB')
+        img_show = img_show.resize(size=(224,224))
+
+
+        ax = axes[i//3][np.mod(i,3)]
+        ax.imshow(img_show)
+        at = ax.imshow(attribution_show, cmap='plasma',alpha=0.7, interpolation='none', vmin=0, vmax=attribution_show.max())
+        ax.set_xlabel(f"Predicted_label: {dataset.classes[preds[i][0]]}\n"
+                      f"True_label: {dataset.classes[class_num]}")
