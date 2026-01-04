1e8edd925104e20cb0258c4b07dbfb70509a7f0a
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2026-01-04 01:53:25 +0100

    WIP: Lime explanation, added get_top_k_predictions and batch_predicts to metric_function.py

--- utils/metric_functions.py
+++ utils/metric_functions.py
@@ -2,9 +2,11 @@ import os
 import copy
 import numpy as np
 import torch
-from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score
-from torch.utils.data import DataLoader, Dataset, Subset
+import torchvision
+from matplotlib import pyplot as plt, transforms
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from torch.utils.data import DataLoader, Subset
+import torch.nn.functional as F
 import pandas as pd
 
 
@@ -91,3 +93,35 @@ def test_on_given_idx(dataset, net_model, transform, idx):
         preds.append(pred)
 
     return preds, pd.DataFrame(Data)
+
+def get_top_k_predictions(probs:np.ndarray, labels:np.ndarray, k:int) -> pd.DataFrame:
+    top_k_indices = np.argsort(probs)[-k:][::-1]
+    top_k_names = [labels[i] for i in top_k_indices]
+    top_k_scores = [probs[i] for i in top_k_indices]
+
+    probs_df = pd.DataFrame(
+        {
+            'classes': top_k_names[:k],
+            'preds': top_k_scores[:k],
+        }
+    )
+
+    return probs_df
+
+def batch_predict_probs(images:np.ndarray, net_model:torchvision.models.resnet.ResNet) -> np.ndarray:
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    predict_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std)])
+
+
+    net_model.cpu().eval()
+    batch = torch.stack([predict_transform(img) for img in images], dim=0)
+    logits = net_model(batch)
+
+    probs = F.softmax(logits, dim=1)
+    return probs.detach().numpy()
